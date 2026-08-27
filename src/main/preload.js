5f262b0f5c073620a4afe6c16e29282c8eb0eaa2
zhangfeng <908980328@qq.com>
2026-08-27 16:48:06 +0800

    feat: expose v0.2 browser status and retry IPC

--- src/main/preload.js
+++ src/main/preload.js
@@ -3,16 +3,26 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('api', {
   listInstances: () => ipcRenderer.invoke('instances:list'),
   createInstance: (name) => ipcRenderer.invoke('instances:create', name),
+
   listChannels: (instanceId) => ipcRenderer.invoke('channels:list', instanceId),
   addChannel: (data) => ipcRenderer.invoke('channels:add', data),
   deleteChannel: (id) => ipcRenderer.invoke('channels:delete', id),
+
   listTasks: (instanceId) => ipcRenderer.invoke('tasks:list', instanceId),
   createTask: (data) => ipcRenderer.invoke('tasks:create', data),
   runTask: (id) => ipcRenderer.invoke('tasks:run', id),
+  retryFailedTask: (id) => ipcRenderer.invoke('tasks:retryFailed', id),
+
   listSelectors: () => ipcRenderer.invoke('selectors:list'),
   saveSelector: (data) => ipcRenderer.invoke('selectors:save', data),
   testSelector: (data) => ipcRenderer.invoke('selectors:test', data),
+
   openLogin: (instanceId) => ipcRenderer.invoke('browser:login', instanceId),
+  getLoginStatus: (instanceId) => ipcRenderer.invoke('browser:status', instanceId),
+
+  listSettings: () => ipcRenderer.invoke('settings:list'),
+  setSetting: (data) => ipcRenderer.invoke('settings:set', data),
+
   pickVideo: () => ipcRenderer.invoke('dialog:video'),
   listLogs: () => ipcRenderer.invoke('logs:list')
 });
