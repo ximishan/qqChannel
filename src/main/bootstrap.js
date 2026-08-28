8e987095cc61a39aa7c29c5dd4cd5301cd72997b
zhangfeng <908980328@qq.com>
2026-08-28 20:15:54 +0800

    feat: load multi-account support

--- src/main/bootstrap.js
+++ src/main/bootstrap.js
@@ -3,5 +3,6 @@ const BrowserManager = require('./browser');
 
 require('./comment-support')(DB, BrowserManager);
 require('./cli-publishing-support')(DB, BrowserManager);
+require('./multi-account-support').installMultiAccountSupport(DB, BrowserManager);
 require('./channel-sync-support');
 require('./main');
