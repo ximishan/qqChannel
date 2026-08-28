12a5fda9d4cbf8ca8058deb8dd27cd85f8db7d4b
zhangfeng <908980328@qq.com>
2026-08-28 19:00:03 +0800

    feat: register QQ channel sync IPC

--- src/main/bootstrap.js
+++ src/main/bootstrap.js
@@ -3,4 +3,5 @@ const BrowserManager = require('./browser');
 
 require('./comment-support')(DB, BrowserManager);
 require('./cli-publishing-support')(DB, BrowserManager);
+require('./channel-sync-support');
 require('./main');
