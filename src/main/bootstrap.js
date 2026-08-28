b0f1d05410893d673616c27b250173d74f06e77b
zhangfeng <908980328@qq.com>
2026-08-28 23:10:42 +0800

    feat: enable global task list filtering

--- src/main/bootstrap.js
+++ src/main/bootstrap.js
@@ -4,6 +4,7 @@ const BrowserManager = require('./browser');
 // 单账号模式：直接使用 tencent-channel-cli 在当前 Windows 用户下的登录态。
 // 不再加载多账号 token/keychain 隔离补丁。
 require('./single-account-support')(DB, BrowserManager);
+require('./task-list-filter-support')(DB);
 require('./comment-support')(DB, BrowserManager);
 require('./cli-publishing-support')(DB, BrowserManager);
 require('./channel-sync-support');
