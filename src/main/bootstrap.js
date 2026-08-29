d3eed22c768376b2b25e835269f44608e92975e7
zhangfeng <908980328@qq.com>
2026-08-29 22:23:37 +0800

    fix: enable comment DOM verification support

--- src/main/bootstrap.js
+++ src/main/bootstrap.js
@@ -4,4 +4,5 @@ const BrowserManager = require('./browser');
 // 多账号 DOM 模式：instances 表中的每条记录都对应一个独立的 Chromium
 // persistent partition。不要加载单账号/CLI 覆盖层，否则多个实例会再次共享登录态。
 require('./comment-support')(DB, BrowserManager);
+require('./comment-dom-fix-support')(DB, BrowserManager);
 require('./main');
